4d2f87a6c342477760229dbeb704012ed282d0ef
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-05-20 16:36:56 +0200

    Prova 1

---
+++ index.php
@@ -0,0 +1,28 @@
+
+
+<?php
+$string = "Prima regola del fight club: non parlare mai del fight club.";
+$censored_str = str_replace ("fight club", "***** ****", $string);
+$string_esplosa = explode ('.', $string)
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bad Words</title>
+</head>
+<body>
+  
+<h2>Frase Integrale:</h2>
+  <p> <?php echo $string; ?> </p>
+  <p> <?php echo strlen($string);?></p>
+
+<h2>Frase Censurata:</h2>
+  <p> <?php echo $censored_str; ?> </p>
+  <p> <?php echo strlen($censored_str);?></p>
+  
+</body>
+</html>
